89f1c77408568be08ce31ee138075c9799121775
TMLG\KFA24632 <k.fall90@hotmail.Fr>
2024-03-28 12:28:57 +0100

    Add Return Files

--- algo/mapper.py
+++ algo/mapper.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-import sys
 wordList = dict()
 file_object = open('test-algo.txt')
-for line in file_object:
-    line = line.strip()
-    words = line.split(',')
-
-
-    print(words[1],words[4],1)
+with open("Result/MapperData.txt", "w") as f:
+    for line in file_object:
+        line = line.strip()
+        words = line.split(',')
+        data = "{}\t{}\t{}\n".format(words[1], words[4], 1)
+        f.write(data)
+f.close()
--- algo/reducer.py
+++ algo/reducer.py
@@ -22,39 +22,44 @@ def ecartType(ValueList, moyList, n, som=0):
     return math.sqrt(ecartype)
 
 
-file_object = open('test-algo.txt')
-for line in file_object:
-    line = line.strip()
+file_object = open('Result/MapperData.txt')
+with open("Result/ReducerData.txt", "w") as f:
+    for line in file_object:
+        line = line.strip()
 
-    age, salaire, counter = line.split('\t', 3)
+        age, salaire, counter = line.split('\t', 3)
 
-    try:
-        salaire = int(salaire)
-    except ValueError:
-        continue
-    try:
-        counter = int(counter)
-    except ValueError:
-        continue
+        try:
+            salaire = int(salaire)
+        except ValueError:
+            continue
+        try:
+            counter = int(counter)
+        except ValueError:
+            continue
+        if current_age == age:
+            sumSalaire += salaire
+            current_counter += counter
+            salaireList.append(salaire)
+        else:
+            if current_age:
+                moyenne_salaire = calculMoyenne(sumSalaire, current_counter)
+                ecartype = ecartType(salaireList, moyenne_salaire, current_counter)
+
+                del salaireList[:]
+
+                data = "{} {} {} {}\n".format(current_age, moyenne_salaire, ecartype, current_counter)
+                f.write(data)
+            current_age = age
+            current_counter = counter
+            sumSalaire = salaire
+            salaireList.append(salaire)
+            ecartype = 0
+
     if current_age == age:
-        sumSalaire += salaire
-        current_counter += counter
-        salaireList.append(salaire)
-    else:
-        if current_age:
-            moyenne_salaire = calculMoyenne(sumSalaire, current_counter)
-            ecartype = ecartType(salaireList, moyenne_salaire, current_counter)
-
-            del salaireList[:]
-            print(current_age, moyenne_salaire, ecartype, current_counter)
-        current_age = age
-        current_counter = counter
-        sumSalaire = salaire
-        salaireList.append(salaire)
-        ecartype = 0
-
-if current_age == age:
-    moyenne_salaire = calculMoyenne(sumSalaire, current_counter)
-    ecartype = ecartType(salaireList, moyenne_salaire, current_counter)
-    del salaireList[:]
-    print(current_age, moyenne_salaire, ecartype, current_counter)
+        moyenne_salaire = calculMoyenne(sumSalaire, current_counter)
+        ecartype = ecartType(salaireList, moyenne_salaire, current_counter)
+        del salaireList[:]
+        data = "{} {} {} {}\n".format(current_age, moyenne_salaire, ecartype, current_counter)
+        f.write(data)
+f.close()
